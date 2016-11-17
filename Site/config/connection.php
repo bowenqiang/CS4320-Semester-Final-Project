6585ead9b06f862283966b6631c189075a9a84e8
root <root@ip-172-31-24-76.us-west-2.compute.internal>
2016-11-17 20:12:39 +0000

    Cleaning up the server. If this breaks anything it is Will's problem, because we think he made these changes.

--- Site/config/connection.php
+++ Site/config/connection.php
@@ -1,4 +1,4 @@
 <?php
 # Database Connection Here...
-$dbc = mysqli_connect('127.0.0.1', 'root', '123456', 'ProjectDB') OR die('Could not connect because: '.mysqli_connect_error());
+$dbc = mysqli_connect('127.0.0.1', 'wb334', '24Npj6v7', 'sdw3mdTestDB') OR die('Could not connect because: '.mysqli_connect_error());
 ?>
