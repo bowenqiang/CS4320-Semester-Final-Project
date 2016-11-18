f123d4b6504b0acc11e3263d35d9dd8d6e618ccc
rennekej@gmail.com <jry83@ip-172-31-24-76.us-west-2.compute.internal>
2016-11-18 23:27:49 +0000

    linking/logical flow update

--- Site/template/navigation.php
+++ Site/template/navigation.php
@@ -1,8 +1,11 @@
 <nav class="indigo" role="navigation">
 	<div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Software Engineering</a>
 	  <ul class="right hide-on-med-and-down">
-	    <li><a href="#">Git Hub</a></li>
-	    <li><a href="logout.php">Logout</a></li>
+	    <li><a href="https://github.com/JustinRenneke/CS4320-Semester-Final-Project/wiki">Git Hub</a></li>
+	    <li><a href="browseManifests.php">Search</a></li>
+        <li><a href="contribute.php">Contribute</a></li>
+        <li><a href="addDataset.php">Upload</a></li>
+        <li><a href="logout.php">Logout</a></li>
 	<!--         <li><input id="search"><i class="material-icons">search</i></li> -->
 	  </ul>
 	
