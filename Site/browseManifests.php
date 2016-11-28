f35f5d2b8c5dddbed74bd3f5398997f0c3c3510d
William Morrison <wmorrison96@yahoo.com>
2016-11-28 17:23:26 -0600

    commit 7

--- Site/browseManifests.php
+++ Site/browseManifests.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION['username'])) {
 
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Software Engineering </title>
+  <title>Software Engineering</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
