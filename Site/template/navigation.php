5a33618e60c934f44adeaf73f43a905885403d58
mts5z9 <mts5z9@mail.Missouri.edu>
2016-11-28 22:38:03 -0600

    change createmanifest link

--- Site/template/navigation.php
+++ Site/template/navigation.php
@@ -10,7 +10,7 @@
 	  <ul class="right hide-on-med-and-down">
 	    <li><a href="https://github.com/JustinRenneke/CS4320-Semester-Final-Project/wiki">Git Hub</a></li>
 	    <li><a href="browseManifests.php">Search</a></li>
-        <li><a href="addDataset.php">Upload</a></li>
+        <li><a href="createManifest.php">Upload</a></li>
         <li><a href="createUser.php">Create Account</a></li>
         <?php
             if($_SESSION['category'] == 'admin'){
@@ -21,16 +21,15 @@
             }else{
 //                echo '<li><a href="login.php">Log in</a></li>';
             }
-            
+
         ?>
           <!--        <li><a href="logout.php">Logout</a></li>-->
 	<!--         <li><input id="search"><i class="material-icons">search</i></li> -->
 	  </ul>
-	
+
 	  <ul id="nav-mobile" class="side-nav">
 	    <li><a href="#">Navbar Link</a></li>
 	  </ul>
 	  <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
 	</div>
 </nav>
-  
