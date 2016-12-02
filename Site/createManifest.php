c09303e966320596c0a10307743f201452c9481d
rennekej@gmail.com <jry83@ip-172-31-24-76.us-west-2.compute.internal>
2016-12-02 04:24:10 +0000

    implement dataset uploads in createManifest.php

--- Site/createManifest.php
+++ Site/createManifest.php
@@ -8,6 +8,7 @@
     include('config/connection.php');
     include('config/setup.php');
     include('checkPerson.php');
+    include("Upload.php");
 
     if ($mysqli->connect_errno) {
         printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -52,27 +53,103 @@
         if(strlen($JsonFile) > 255){
             echo "<script type='text/javascript'>alert('ERROR: JSON File URL cannot be > 255 chars')</script>";
         }        
-        $DataSet = htmlspecialchars($_POST['DataSet']);
-        if(strlen($DataSet) > 255 || strlen($DataSet) == 0){
-            echo "<script type='text/javascript'>alert('ERROR: Dataset URL cannot be > 255 or 0 chars')</script>";
-        }        
+        $DataSet = "placeholder";
+//        $DataSet = htmlspecialchars($_POST['DataSet']);
+//        if(strlen($DataSet) > 255 || strlen($DataSet) == 0){
+//            echo "<script type='text/javascript'>alert('ERROR: Dataset URL cannot be > 255 or 0 chars')</script>";
+//        }        
 
         //$sql = "SELECT PID FROM person WHERE FirstName='$FirstName' AND LastName='$LastName'";
 
         $PID = checkPerson($dbc, $FirstName, $LastName); //from checkPerson.php
         $Creator = $PID; //just to make it obvious in the sql statement
-        
-	$sql = "INSERT INTO manifest VALUES('$StandardVersions', DEFAULT, $Creator, now(), 
+        $sql = "INSERT INTO manifest VALUES('$StandardVersions', DEFAULT, $Creator, now(), 
                 '$UploadComment', '$UploadTitle', '$DsTitle', '$DsTimeInterval', '$RetrievedTimeInterval', 
                 '$DsDateCreated', '$JsonFile', '$DataSet')"; //DEFAULT for MID since it auto-increments; can be changed depending on final implementation
-        
-	if($result = mysqli_query($dbc, $sql)){	//Should test this for success
-                echo "<script type='text/javascript'>alert('Manifest created! Redirecting...')</script>";
+        if($result = mysqli_query($dbc, $sql)){	//Should test this for success
+//                echo "<script type='text/javascript'>alert('Manifest created! Redirecting...')</script>";
+
+
+            //UPLOAD THE FILE
+            if($_FILES['file1']){
+                
+                //we need the MID from the manifest's database entry to create a unique folder for its associated files
+                
+                $sql = "SELECT MID FROM manifest WHERE UploadTitle='$UploadTitle'"; 
+                if($result = mysqli_query($dbc, $sql)){
+                    $data=mysqli_fetch_assoc($result);
+                }else{
+                    echo "<script type='text/javascript'>alert('Database error! Manifest creation failed!')</script>";
+                }
+                $mid = $data['MID'];
+                    
+                $target_dir = "../DatasetFiles/" . $mid;
+
+                try {
+                    $upload = new Upload('file1');
+                    
+                    
+                    $fileExt = $upload->getFileExt();
+                    $fileSize = $upload->getfileSize();
+                    
+                    //the default max upload allowed by php is 2 MB, or 2097152 bytes
+                    if($fileSize > 2097152){
+                        die("That file is too big!");
+                    }  
+                    if($fileSize == 0){
+                        die("Files of size 0 are invalid!");
+                    }
+                    //try to protect against dangerous file extensions. Probably useless, but hey I tried.
+                    if($fileExt == 'exe'){
+                        die("Invalid file extension!");
+                    }
+                    
+                    //temporarily set the umask so we can give any newly created directly open permissions (if we don't do this permissions = 777-22 = 755)
+                    $oldmask = umask(0);
+
+                    if(!is_dir($target_dir) && !mkdir($target_dir, 0777)){
+                        die("error creating folder $target_dir");
+                    }
+                    
+                    umask($oldmask);
+                    
+                    //create destination
+                    $destFilePath = $target_dir . '/test.' . $fileExt;
+
+                    $upload -> moveFile($destFilePath); //call from upload.php
+                    
+                    $sql = "UPDATE manifest SET DataSet='$destFilePath' WHERE MID='$mid'"; 
+                    if($result = mysqli_query($dbc, $sql)){
+                        $data=mysqli_fetch_assoc($result);
+                    }else{
+                        echo "<script type='text/javascript'>alert('Database error! Manifest creation failed!')</script>";
+                    }
+
+                }catch(UploadExceptionNoFile $e){
+                    print "no file was uploaded.<br>\n";
+                    $code = $e->getCode();
+                    $message = $e->getMessage();
+                    print "Error: $message (code = $code) <br>\n";
+                }
+
+                //catch any other exceptions
+                catch(UploadException $e){
+                    $code = $e->getCode();
+                    $message = $e->getMessage();
+                    print "Error: $message (code = $code) <br>\n";
+                }
+                //END FILE UPLOAD BLOCK
+                echo "<script type='text/javascript'>alert('Manifest created with associated dataset! Redirecting...')</script>";
                 echo "<script type='text/javascript'>window.location = 'browseManifests.php'</script>";
+            }else{
+                echo "<script type='text/javascript'>alert('Manifest created without associating a dataset! Redirecting...')</script>";
+                echo "<script type='text/javascript'>window.location = 'browseManifests.php'</script>";
+            }
         }else{
                echo "<script type='text/javascript'>alert('Database insertion error! Manifest creation failed!')</script>";
                printf("dbc error: %s\n", $dbc->error);
         }
+            
 /* //this block commented out with implementation of checkPerson function
         if($result = mysqli_query($dbc, $sql)){
             if(mysqli_num_rows($result)) {
@@ -121,7 +198,7 @@
       			<h3>Create Manifest</h3>
       		</div>
 
-				<form action="createManifest.php" method="post" role="form">
+				<form action="createManifest.php" method="post" role="form" enctype="multipart/form-data">
 					<div class="input-field col s12 form-group">						
 						<label for="FirstName" >Author's First Name</label>
 						<input id="FirstName" type="text" class="validate" name="FirstName">
@@ -162,10 +239,29 @@
 						<label for="JsonFile" >JSON File URL</label>
 						<input id="JsonFile" type="text" class="validate" name="JsonFile">
 			    	</div>
+<!--
 					<div class="input-field col s12 form-group">						
 						<label for="DataSet" >DataSet URL</label>
 						<input id="DataSet" type="text" class="validate" name="DataSet">
 			    	</div>
+-->
+                    <div class="row">
+<!--                        <form action="#">-->
+                            <div class="input-field col s12">
+                                <div class="file-field input-field">
+                                    <div class="btn">
+                                        <span>Dataset File</span>
+                                        <input type="file" name="file1">
+                                    </div>
+                                    <div class="file-path-wrapper">
+                                        <input class="file-path validate" type="text" placeholder="Upload a Dataset">
+                                    </div>
+                                </div>
+                            </div>
+<!--                        </form>-->
+                    </div>
+                    
+                    
                     
 					<div class="col s4">
 						<button type="submit" value="submit" class="waves-effect waves-light btn">Create</button>
