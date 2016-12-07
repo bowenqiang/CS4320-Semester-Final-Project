2c8eae2926a1d5aec7949397be8904623a5307b7
rennekej@gmail.com <jry83@ip-172-31-24-76.us-west-2.compute.internal>
2016-12-07 21:27:18 +0000

    cosmetics

--- Site/admin/config/connection.php
+++ Site/admin/config/connection.php
@@ -1,4 +1,4 @@
 <?php
 # Database Connection Here...
-$dbc = mysqli_connect('127.0.0.1', 'root', '123456', 'project') OR die('Could not connect because: '.mysqli_connect_error());
+$dbc = mysqli_connect('127.0.0.1', 'root', 'W13k39zzz', 'sdw3mdTestDB') OR die('Could not connect because: '.mysqli_connect_error());
 ?>
