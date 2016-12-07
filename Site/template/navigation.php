f0d28c37c0966614fb27b063d251751c686e70d9
Wenqiang Bo <bowenqiang@live.com>
2016-12-07 16:07:04 -0600

    add validation when create new user and minor change to navgation

--- Site/template/navigation.php
+++ Site/template/navigation.php
@@ -24,7 +24,7 @@
             if(isset($_SESSION['username'])) {
                 echo '<li><a href="logout.php">Log out</a></li>';
             }else{
-//                echo '<li><a href="login.php">Log in</a></li>';
+                echo '<li><a href="login.php">Log in</a></li>';
             }
             
         ?>
